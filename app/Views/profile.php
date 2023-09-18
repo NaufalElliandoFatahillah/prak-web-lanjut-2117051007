7a042b8ddbd48aa4ac62b33bdd8b4d96e40f7a1e
NaufalElliandoFatahillah <naufal.elliando21@students.unila.ac.id>
2023-09-18 21:10:40 +0700

    Routing And View

---
+++ app/Views/profile.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="text-center">
+<img src="<?=base_url();?>image/ppcisco.jpeg" class="rounded-circle" alt="..." width="200" height="200">
+<table class="mx-auto">
+<p>
+<tr>
+  <td>
+    <button class="p-2 mt-2 bg-secondary text-white" style="width: 300px;">Naufal Elliando Fatahillah</button>
+  </td>
+</tr>
+<tr>
+  <td>
+<button class="p-2 mt-2 bg-secondary text-white" style="width: 300px;">AB</button>
+</td>
+</tr>
+<tr class="text-center">
+  <td>
+<button class="p-2 mt-2 bg-secondary text-white" style="width: 300px;">2117051007</button>
+  </td>
+</tr>
+</p>
+</table>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</div>
+</body>
+</html>
