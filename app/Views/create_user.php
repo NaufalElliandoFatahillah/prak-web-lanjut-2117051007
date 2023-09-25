4748d12e0f06f21ce446378cfe1c29b7eaa5e121
NaufalElliandoFatahillah <naufal.elliando21@students.unila.ac.id>
2023-09-25 21:30:48 +0700

    feat:add form

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -97,7 +97,17 @@
         </div>
         <div class="form-group">
           <label for="kelas">Kelas :</label>
-          <input type="text" class="form-control" name="kelas" placeholder="Masukkan Kelas">
+          <select class="form-control" name="kelas" id="kelas">
+            <?php 
+            foreach ($kelas as $item){
+              ?>
+                  <option value="<?= $item['id'] ?>">
+                  <?= $item['nama_kelas'] ?>
+                  </option>
+                <?php
+            }
+            ?>
+          </select>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
