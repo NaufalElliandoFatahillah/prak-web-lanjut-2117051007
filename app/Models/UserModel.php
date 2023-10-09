9d018021e5f700397cb0b9c93b45a8b1669d13e7
NaufalElliandoFatahillah <naufal.elliando21@students.unila.ac.id>
2023-10-09 18:51:56 +0700

    uploud-file

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -13,7 +13,7 @@ class UserModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['nama', 'npm', 'id_kelas'];
+    protected $allowedFields    = ['nama', 'npm', 'id_kelas', 'foto'];
 
     // Dates
     protected $useTimestamps = false;
