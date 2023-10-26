a6cbfc05b0b8435196f06b15c86542483985c885
NaufalElliandoFatahillah <naufal.elliando21@students.unila.ac.id>
2023-10-27 00:27:57 +0700

    update_delete

--- app/Views/list_user.php
+++ app/Views/list_user.php
@@ -33,8 +33,12 @@
                         <td><?= $u['nama_kelas']; ?></td>
                         <td>
                             <a class="btn btn-primary" href="<?= base_url('user/' . $u['id']) ?>">Detail</a>
-                            <a class="btn btn-warning" href="">Edit</a>
-                            <a class="btn btn-danger"   href="">Hapus</a>
+                            <a class="btn btn-warning" href="<?= base_url('user/' . $u['id']. '/edit') ?>">Edit</a>
+                            <form action="<?=base_url('user/' . $u['id'])?>" method="POST">
+                            <input type="hidden" name="_method" value="Delete">
+                            <?= csrf_field() ?>
+                            <input type="submit" class="btn btn-danger" value="Hapus">
+                            </form>
                         </td>
                     </tr>
                     <!-- Modal for Delete Confirmation -->
